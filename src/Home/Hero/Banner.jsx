fbb1b7aed095f80dde4c61c85c5c409d2ec4f298
mdshakil04 <m.shakil04@yahoo.com>
2025-06-28 21:07:49 +0600

    Hero section done

--- src/Home/Hero/Banner.jsx
+++ src/Home/Hero/Banner.jsx
@@ -1,52 +1,21 @@
-import React from "react";
-import banner1 from "../../assets/images/banner/banner-01.jpg";
-import banner2 from "../../assets/images/banner/banner-02.jpg";
 import banner3 from "../../assets/images/banner/banner-03.jpg";
 const Banner = () => {
   return (
-    <div className="carousel w-full lg:h-[850px]">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={banner1} className="w-full" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={banner2} className="w-full" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={banner3} className="w-full" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={banner2} className="w-full" />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${banner3})`,
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content ">
+        <div className="max-w-md">
+          <button className="btn btn-primary">Get Started</button>
+          <h1 className="mb-5 text-5xl font-bold">
+            HURRY!
+            <br /> GET THE BEST
+            <br /> VILLA FOR YOU
+          </h1>
         </div>
       </div>
     </div>
